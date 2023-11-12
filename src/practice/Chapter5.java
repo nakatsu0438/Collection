f88bbe3447d57abacab3cc51bdf12c64a85dd97b
YutaNakatsu <https://github.com/nakatsu0438/eclipse-test.git>
2023-11-12 19:45:15 +0900

    課題再提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -12,13 +12,12 @@ public class Chapter5 {
         tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接"));
         tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
         tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
-        tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く."));
+        tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
 
         Collections.sort(tasks);// タスクを時系列に並び替え
 
         for (Task task : tasks) {
             System.out.println(task);
         }
-        System.out.println("\nタスクは残り" + tasks.size());
     }
 }
